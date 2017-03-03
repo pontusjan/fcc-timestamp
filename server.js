33c1857a359db44a0beebb5adcc76b543e18a3f4
Pontus <pontus.jansson91@gmail.com>
2017-03-03 07:23:24 +0100

    API added and tested

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -6,11 +7,25 @@ var port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use('/assets/', express.static(__dirname + '/public'));
 
-
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('index', {theUrl: req.url});
 });
 
+app.use('/:id', function(req, res){
+    var timeInUnix = new Date(Number(req.params.id)).getTime() || new Date(req.params.id).getTime();
+    var timeInLang = moment(timeInUnix).format('MMMM D, YYYY');
+    if (timeInUnix){
+        res.json({
+            unix: timeInUnix,
+            natural: timeInLang
+        })
+    } else {
+        res.json({
+            unix: null,
+            natural: null
+        });
+    }
+})
 
 app.listen(port, function(){
     console.log('Express app started, listeing on port ' + port);
